e530f56ea471b73968686d67895ace0fca8b2767
borded2dev <bored2dev@gmail.com>
2026-02-20 03:08:29 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function getProfile(){
+	const username  = document.getElementById("usernameInput").value;
+	fetch(`https://api.scratch.mit.edu/users/${username}`)
+		.then(res => res.json())
+		.then(data => {
+			document.getElementById("profileCard").innerHTML = `Username: ${data.username} <br>
+         ID: ${data.id}`;
+		});
+}
